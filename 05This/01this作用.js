8b7acc04ea2d7b03df8816ed043f1b3f8cf77177
dang <862294826@qq.com>
2022-04-15 17:42:03 +0800

    this 绑定规则

--- 05This/01this作用.js
+++ 05This/01this作用.js
@@ -23,3 +23,76 @@ obj.eating()  //dang
     foo.call('abc') //abc
 
 }
+
+{
+    // 默认绑定  显示绑定  隐示绑定 new
+    // 1
+    function foo () { }
+    foo() //独立函数调用 this 指向window
+
+    //2 独立函数调用
+    function foo1 () {
+        console.log(this);//window
+    }
+    function foo2 () {
+        console.log(this);//window
+        foo1()
+    }
+    function foo3 () {
+        console.log(this);//window
+        foo2()
+    }
+    foo3()
+
+    // 3
+    function foo4 () {
+        return function () { console.log(this); }
+    }
+    let fn = foo4();
+    fn() //window
+
+    // 4
+    let obj = { name: "dang", foo: function () { console.log(this) } }
+    let foo5 = obj.foo
+    foo5() //window
+
+    //5
+    function foo6 () { console.log(this); }
+    let obj1 = { name: "dang", foo: foo6 }
+    let fn2 = obj1.foo
+    fn2() //window
+
+
+}
+{
+    // 隐式绑定
+    // 通过某个对象发起调用 1
+    function foo () { console.log(this); }
+    let obj = { foo: foo }
+    obj.foo()//window
+
+    // 2
+    let obj1 = { name: "obj1", foo: function () { console.log(this); } }
+    let obj2 = { name: "obj2", bar: obj1.foo }
+    obj2.bar()//obj2
+}
+{
+    // 显示绑定 call apply   bind
+    function foo () { console.log(this); }
+    let obj = { name: 'obj' }
+    foo.call(obj) //obj
+    foo.apply(obj)//obj
+
+    function bar () { console.log(this); }
+    let newBar = foo.bind(obj)
+    // 默认绑定和显示绑定  显示绑定优先级高
+    newBar()//obj
+}
+{
+    // new 绑定
+    function Foo () { } //类
+    new Foo()
+    // new 开辟一个新内存
+    // 创建一个新对象 将对象和this绑定
+    // 返回这个对象
+}
